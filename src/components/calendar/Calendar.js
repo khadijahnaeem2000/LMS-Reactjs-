83d92452954a68c29387fb245945b469beea000f
Khadija Saeed <khadijasaeed17@yahoo.com>
2023-04-27 19:21:44 +0500

    adding calendar

--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -8,8 +8,6 @@ import {useMemo, useEffect, useState} from 'react';
 import { es } from 'date-fns/locale';
 import toDate from 'date-fns/toDate'
 import parseISO from 'date-fns/parseISO'
-import { endPoint } from '../../config/config';
-
 
 import './styles.css'
 import { getLocalUserdata } from 'services/auth/localStorageData'
@@ -31,10 +29,23 @@ const localizer = dateFnsLocalizer({
 
 const MyCalendar = (props) => {
   const [schedule, setSchedule] = useState([]);
+  const lang = {
+    es: {
+      week: 'Semana',
+      work_week: 'Semana de trabajo',
+      day: 'Día',
+      month: 'Mes',
+      previous: 'Atrás',
+      next: 'Siguiente',
+      today: 'Hoy',
+      agenda: 'El Diario',
+      
+      showMore: (total) => `+${total} más`,
+    }
+  }
 
   useEffect(() => {
     const data = getLocalUserdata();
-
     userServices.commonPostService('/GetSchedule',{"studentId":data.id,"month":(new Date().getMonth()+1)})
     .then((response) => {
       if(response.data.status==='Successfull')
@@ -59,90 +70,71 @@ const MyCalendar = (props) => {
     });
   },[])
 
-  const lang = {
-    es: {
-      week: 'Semana',
-      work_week: 'Semana de trabajo',
-      day: 'Día',
-      month: 'Mes',
-      previous: 'Atrás',
-      next: 'Siguiente',
-      today: 'Hoy',
-      agenda: 'El Diario',
-      
-      showMore: (total) => `+${total} más`,
-    }}
+  const { defaultDate, messages, views, formats } = useMemo(() => ({
+    defaultDate: new Date(),
+    messages: lang['es'],
+    views : {
+      month: true,
+      week: true
+    },
+    formats: {
+      dayRangeHeaderFormat: ({ start, end }, culture, localizer) =>
+        localizer.format(start, 'dd', culture) +
+        ' - ' +
+        localizer.format(end, 'dd LLLL yyyy', culture),
+      dayHeaderFormat: (date, culture, localizer) =>
+        localizer.format(date, 'EEEE, dd MMMM yyyy', culture),
+    },
+  }),[])
 
-    {/*const d = new Date();
-    const localTime = d.getTime();
-    const localOffset = d.getTimezoneOffset() * 60000;
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    const style = {
+        backgroundColor: event?.bgColor,
+    };
+    return {
+        style: style
+    };
+  }
 
-    const utc = localTime + localOffset;
-    const offset = 2; // UTC of Dubai is +04.00
-    const dubai = utc + (3600000 * offset);
-
-    const spainTimeNow = new Date(dubai).toLocaleString();
-    console.log(new Date(spainTimeNow));*/}
-
-    const { defaultDate, messages, views, formats } = useMemo(() => ({
-      defaultDate: new Date(),
-      messages: lang['es'],
-      views : {
-        month: true,
-        week: true
-      },
-      formats: {
-        dayRangeHeaderFormat: ({ start, end }, culture, localizer) =>
-          localizer.format(start, 'dd', culture) +
-          ' - ' +
-          localizer.format(end, 'dd LLLL yyyy', culture),
-        dayHeaderFormat: (date, culture, localizer) =>
-          localizer.format(date, 'EEEE, dd MMMM yyyy', culture),
-      },
-    }),[])
-
-    const eventStyleGetter = (event, start, end, isSelected) => {
-      var style = {
-          backgroundColor: event?.bgColor,
-      };
-      return {
-          style: style
-      };
+  const renderTitle = (event) => {
+    return (
+      <span className='calendar-event-cell'>
+      {
+        event?.image
+        ?<img className='calendar-event-cell-image' src={`https://neoestudio.net/${event?.image}`} alt=''/>
+        :null
+      }
+        <span>{event.title}</span>
+      </span>
+    )
   }
   
-  return(
-    <div style={{display:'flex', width:'96%', margin:'2%'}}>
-      <div style={{flexGrow:1}}>
-      <Calendar
-        localizer={localizer}
-        events={schedule}
-        startAccessor="start"
-        endAccessor="end"
-        style={{ height: 550 }}
-        culture={'es'}
-        messages={messages}
-        defaultDate={defaultDate}
-        views={views}
-        popup
-        components={{
-          week: {
-            header: ({ date, localizer }) => localizer.format(date, 'EEEE dd')
-          },
-        }}
-        formats={formats}
-        eventPropGetter={(eventStyleGetter)}
-        titleAccessor={function(e){return <span style={{display:'flex'}}>
-          {e?.image?
-            <img style={{maxWidth:'3em', borderRadius:'3%'}} src={`https://neoestudio.net/${e?.image}`}/>
-            :null}
-                      <span>{e.title}</span>
-            </span>;
+  return (
+    <div className='calendar-container'>
+      <div className='calendar-container-child'>
+        <Calendar
+          localizer={localizer}
+          events={schedule}
+          startAccessor="start"
+          endAccessor="end"
+          style={{ height: 550 }}
+          culture={'es'}
+          messages={messages}
+          defaultDate={defaultDate}
+          views={views}
+          popup
+          components={{
+            week: {
+              header: ({ date, localizer }) => localizer.format(date, 'EEEE dd')
+            },
           }}
-
-      />
+          formats={formats}
+          eventPropGetter={(eventStyleGetter)}
+          titleAccessor={renderTitle}
+        />
       </div>
     </div>
   )
 }
 
-    export default MyCalendar
+export default MyCalendar;
